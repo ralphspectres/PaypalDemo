00a6a3c158e0aba65b89453fc6be58ecf354a40e
ralphlaurenagustin <ralphlauren.agustin@benilde.edu.ph>
2017-09-07 11:07:34 +0800

    PaypalDemo

---
+++ PaypalMVC/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaypalMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult PaypalView(string item, string amount)
+        {
+            PaypalMVC.Models.PaypalModel paypal = new Models.PaypalModel();
+            paypal.cmd = "_xclick";
+            paypal.business = ConfigurationManager.AppSettings["BusinessAccountKey"];
+
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["UseSandbox"]);
+            if(useSandbox)
+            {
+                ViewBag.actionURL = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+            }
+            else
+            {
+                ViewBag.actionURL = "https://www.paypal.com/cgi-bin/webscr";
+            }
+
+            paypal.cancel_return = System.Configuration.ConfigurationManager.AppSettings["CancelURL"];
+            paypal.@return = ConfigurationManager.AppSettings["ReturnURL"];
+            paypal.notify_url = ConfigurationManager.AppSettings["NotifyURL"];
+            paypal.currency_code = ConfigurationManager.AppSettings["CurrencyCode"];
+
+            paypal.item_name = item;
+            paypal.amount = amount;
+            return View(paypal);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PaypalMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PaypalMVC.Startup))]
+namespace PaypalMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
